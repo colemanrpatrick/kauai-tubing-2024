0621f4beb92a58f764c284cf07b0c6941b892694
patrick <17029142+colemanrpatrick@users.noreply.github.com>
2024-05-02 23:08:10 -0400

    next-update

---
+++ js/home.js
@@ -0,0 +1,50 @@
+const sliderLeft = document.getElementById("slider-left");
+const sliderRight = document.getElementById("slider-right");
+let slides = document.getElementsByClassName("hero-slide");
+let dots = document.getElementsByClassName("slider-button");
+let slideIndex;
+
+slideIndex = 0;
+
+let createSlideImage = (el,index) => {
+    if(!el.querySelector('IMG')){
+        let dataImg = el.getAttribute("data-img");
+        let slideImage = document.createElement("img");
+       
+        slideImage.setAttribute("class","");
+        slideImage.src = dataImg;
+        slideImage.setAttribute("id","slide-image-" + index);
+        el.appendChild(slideImage);
+    };
+};
+
+createSlideImage(slides[0],0);
+
+let plusSlides = (n) => { 
+    showSlides(slideIndex += n); 
+};
+
+let currentSlide = (n) => {
+    showSlides(slideIndex = n);
+};
+
+let showSlides = (n) => {
+    let i;
+    if (n > slides.length - 1) { slideIndex = 0 };
+    if (n < 0) { slideIndex = slides.length - 1; };
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = "hero-slide";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = "slider-button";
+    }
+    slides[slideIndex].className = "hero-slide active";
+    dots[slideIndex].className = "slider-button active";
+    createSlideImage(slides[slideIndex]);
+}; 
+
+currentSlide(0);
+
+setTimeout(() => { plusSlides(1) }, "2000");
+let carousel = setInterval(() => {plusSlides(1)},"5000");
+carousel;
